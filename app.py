32aa3517760f2b5f3484a95432f964dd1cd80cdd
Ankit Raj <ankitraj84097@gmail.com>
2026-04-13 18:56:30 +0530

    new version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello from Ankit's CI-CD."
+    return "Hello from Ankit's CI-CD version2."
 
 if __name__ == "__main__":
     # Cloud Run/GCP expects the app to listen on the port defined by the PORT env var
